56165ec4dcf009e9da24fc54c9c496053f67e3c7
foreboding-sky <andriy.kondratuik@gmail.com>
2020-06-19 23:51:29 +0300

    Added target prefab, Results UI.

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+   [SerializeField]
+    private float lifeTime = 1f;
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+}
